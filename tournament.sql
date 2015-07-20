b0c233e37aac0c767a23f93344348f9f2f92a029
yaasur <yaasur@users.noreply.github.com>
2015-07-19 20:08:59 -0400

    feat: Database structure for tournament project.

--- tournament.sql
+++ tournament.sql
@@ -5,5 +5,15 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+CREATE TABLE player
+(player_id serial PRIMARY KEY,
+name char(50),
+wins Integer,
+matches_played Integer,
+bye Boolean);
 
-
+CREATE TABLE matches
+(match_id serial primary key,
+match_round Integer,
+player1_id serial references player(player_id),
+player2_id serial references player(player_id));
